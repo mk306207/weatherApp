dee520da8d0f943b258d15b5e0c7d2e2bd46f051
mk306207 <kolbermateusz@gmail.com>
2025-04-30 11:59:53 +0200

    icon

--- myApp/weather/static/js/getDeafultCities.js
+++ myApp/weather/static/js/getDeafultCities.js
@@ -6,6 +6,12 @@ async function loadTemperature(){
     const response = await fetch('/realtime-weather');
     const data = await response.json();
     data.forEach(City => {
-        console.log(City.location.name)
+        const id = City.location.name
+        const container = document.getElementById(id);
+        const img = document.createElement('img');
+        img.style.width = '64px';
+        img.style.height = '64px';
+        img.src = City.current.condition.icon;
+        container.appendChild(img);
     });
 }
