c75f34da3d6e14ef697801ee6faacb010bff7d78
mk306207 <kolbermateusz@gmail.com>
2025-05-08 11:29:55 +0200

    last test

--- myApp/weather/tests.py
+++ myApp/weather/tests.py
@@ -32,3 +32,154 @@ class TestAPICall(TestCase):
         response = response.json()
         self.assertEqual(response[0]["location"]["name"],"Honolulu")
         self.assertEqual(response[1]["location"]["name"],"NewYork")
+        
+    @patch('weather.views.requests.get')
+    def testForecastWeather(self, mockGet):
+        mockResponse1 = Mock()
+        mockResponse1.json.return_value={
+                                            "location": {
+                                                "name": "Honolulu"
+                                            },
+                                            "forecast": {
+                                                "forecastday": [
+                                                {
+                                                    "date": "2025-05-08",
+                                                    "date_epoch": 1746662400,
+                                                    "day": {
+                                                        "maxtemp_c": 17.4,
+                                                        "maxtemp_f": 63.4,
+                                                        "mintemp_c": 5.7,
+                                                        "mintemp_f": 42.3,
+                                                        "avgtemp_c": 11.5,
+                                                        "avgtemp_f": 52.6,
+                                                        "maxwind_mph": 9.4,
+                                                        "maxwind_kph": 15.1,
+                                                        "totalprecip_mm": 0.03,
+                                                        "totalprecip_in": 0.0,
+                                                        "totalsnow_cm": 0.0,
+                                                        "avgvis_km": 10.0,
+                                                        "avgvis_miles": 6.0,
+                                                        "avghumidity": 64,
+                                                        "daily_will_it_rain": 0,
+                                                        "daily_chance_of_rain": 0,
+                                                        "daily_will_it_snow": 0,
+                                                        "daily_chance_of_snow": 0,
+                                                        "condition": {
+                                                            "text": "Partly Cloudy ",
+                                                            "icon": "//cdn.weatherapi.com/weather/64x64/day/116.png",
+                                                            "code": 1003
+                                                        },
+                                                        "uv": 1.4
+                                                    }
+                                                },
+                                                {
+                                                    "date": "2025-05-09",
+                                                    "date_epoch": 1746662400,
+                                                    "day": {
+                                                        "maxtemp_c": 27.4,
+                                                        "maxtemp_f": 93.4,
+                                                        "mintemp_c": 4.7,
+                                                        "mintemp_f": 65.3,
+                                                        "avgtemp_c": 77.5,
+                                                        "avgtemp_f": 123.6,
+                                                        "maxwind_mph": 123.4,
+                                                        "maxwind_kph": 123.1,
+                                                        "totalprecip_mm": 0.03,
+                                                        "totalprecip_in": 0.0,
+                                                        "totalsnow_cm": 0.0,
+                                                        "avgvis_km": 10.0,
+                                                        "avgvis_miles": 6.0,
+                                                        "avghumidity": 64,
+                                                        "daily_will_it_rain": 0,
+                                                        "daily_chance_of_rain": 0,
+                                                        "daily_will_it_snow": 0,
+                                                        "daily_chance_of_snow": 0,
+                                                        "condition": {
+                                                            "text": "Partly Cloudy ",
+                                                            "icon": "//cdn.weatherapi.com/weather/64x64/day/116.png",
+                                                            "code": 1003
+                                                        },
+                                                        "uv": 1.4
+                                                    }
+                                                }
+                                                ]
+                                            }
+                                            }
+        mockResponse2 = Mock()
+        mockResponse2.json.return_value = {
+                                            "location": {
+                                                "name": "New York"
+                                            },
+                                            "forecast": {
+                                                "forecastday": [
+                                                {
+                                                    "date": "2025-05-08",
+                                                    "date_epoch": 1746662400,
+                                                    "day": {
+                                                    "maxtemp_c": 17.4,
+                                                    "maxtemp_f": 63.4,
+                                                    "mintemp_c": 5.7,
+                                                    "mintemp_f": 42.3,
+                                                    "avgtemp_c": 11.5,
+                                                    "avgtemp_f": 52.6,
+                                                    "maxwind_mph": 9.4,
+                                                    "maxwind_kph": 15.1,
+                                                    "totalprecip_mm": 0.03,
+                                                    "totalprecip_in": 0.0,
+                                                    "totalsnow_cm": 0.0,
+                                                    "avgvis_km": 10.0,
+                                                    "avgvis_miles": 6.0,
+                                                    "avghumidity": 64,
+                                                    "daily_will_it_rain": 0,
+                                                    "daily_chance_of_rain": 0,
+                                                    "daily_will_it_snow": 0,
+                                                    "daily_chance_of_snow": 0,
+                                                    "condition": {
+                                                        "text": "Partly Cloudy ",
+                                                        "icon": "//cdn.weatherapi.com/weather/64x64/day/116.png",
+                                                        "code": 1003
+                                                    },
+                                                    "uv": 1.4
+                                                    }
+                                                },
+                                                {
+                                                    "date": "2025-05-09",
+                                                    "date_epoch": 1746662400,
+                                                    "day": {
+                                                    "maxtemp_c": 27.4,
+                                                    "maxtemp_f": 93.4,
+                                                    "mintemp_c": 4.7,
+                                                    "mintemp_f": 65.3,
+                                                    "avgtemp_c": 77.5,
+                                                    "avgtemp_f": 123.6,
+                                                    "maxwind_mph": 123.4,
+                                                    "maxwind_kph": 123.1,
+                                                    "totalprecip_mm": 0.03,
+                                                    "totalprecip_in": 0.0,
+                                                    "totalsnow_cm": 0.0,
+                                                    "avgvis_km": 10.0,
+                                                    "avgvis_miles": 6.0,
+                                                    "avghumidity": 64,
+                                                    "daily_will_it_rain": 0,
+                                                    "daily_chance_of_rain": 0,
+                                                    "daily_will_it_snow": 0,
+                                                    "daily_chance_of_snow": 0,
+                                                    "condition": {
+                                                        "text": "Partly Cloudy ",
+                                                        "icon": "//cdn.weatherapi.com/weather/64x64/day/116.png",
+                                                        "code": 1003
+                                                    },
+                                                    "uv": 1.4
+                                                    }
+                                                }
+                                                ]
+                                            }
+                                        }
+        mockGet.side_effect = [mockResponse1,mockResponse2]
+        response = self.client.get(reverse('forecast_weather'))
+        response = response.json()
+        print(response)
+        self.assertEqual(response[0]["location"],"Honolulu")
+        self.assertEqual(response[1]["location"],"New York")
+        self.assertEqual(response[0]["forecast"][0]["date"],"05-08")
+        self.assertEqual(response[1]["forecast"][0]["date"],"05-08")
